2a300cf8fe588cfe38b0cef8bd1677a4142aa669
IHaidov <vanya.haidov@gmail.com>
2023-09-28 13:25:13 +0200

    Direct log messages

---
+++ T04/EmitLogsDirect/EmitLogsDirect.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct_logs1", type: ExchangeType.Direct);
+
+var severity = (args.Length > 0) ? args[0] : "info";
+var message = (args.Length > 1)
+                    ? string.Join(" ", args.Skip(1).ToArray())
+                    : "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(exchange: "direct_logs1",
+                     routingKey: severity,
+                     basicProperties: null,
+                     body: body);
+Console.WriteLine($" [x] Sent '{severity}':'{message}'");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
